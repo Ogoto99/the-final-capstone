530576f3fe2c48e7661126e19074ee6e073ec535
Ogoto <ogotomelekh99@gmail.com>
2025-03-25 20:51:09 +0300

    added some changes

---
+++ script.js
@@ -0,0 +1,6 @@
+function welcomeMsg() {
+    // Display pop-up message
+    alert("You have successfully created an account! Press OK to be redirected to the home page.");
+    // Redirect to the home page
+            window.location.href = "index.html"; // Replace 'home.html' with the URL of your home page.
+        }
